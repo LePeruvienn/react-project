a1e8cac553af87f708195ee053d63e224bd0b735
LePeruvienn <koto91@free.fr>
2024-08-23 23:29:23 +0200

    Updating Contact page

--- src/components/contact/Form.tsx
+++ src/components/contact/Form.tsx
@@ -2,8 +2,27 @@
 function Form () {
 
 	return (
-		<div className="w-full h-168">
-			Form
+		<div class="py-8 lg:py-16 px-4 mx-auto max-w-screen-md">
+			<p class="mb-8 lg:mb-16 font-light text-center text-gray-500  sm:text-xl">Got a technical issue? Want to send feedback about a beta feature? Need details about our Business plan? Let us know.</p>
+			<form action="#" class="space-y-8">
+				<div>
+					<label for="email" class="block mb-2 text-lg font-medium text-gray-900 ">Your email</label>
+					<input type="email" id="email" class="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-red-500 focus:border-red-500 block w-full p-2.5" placeholder="name@flowbite.com" required/>
+				</div>
+				<div>
+					<label for="subject" class="block mb-2 text-lg font-medium text-gray-900 ">Subject</label>
+					<input type="text" id="subject" class="block p-3 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 shadow-sm focus:ring-red-500 focus:border-red-500" placeholder="Let us know how we can help you" required/>
+				</div>
+				<div class="sm:col-span-2">
+					<label for="message" class="block mb-2 text-lg font-medium text-gray-900 ">
+						Your message
+					</label>
+					<textarea id="message" rows="6" class="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg shadow-sm border border-gray-300 focus:ring-red-500 focus:border-red-500" placeholder="Leave a comment..."></textarea>
+				</div>
+				<button type="submit" class="py-3 px-5 text-lg font-medium text-center text-white rounded-lg bg-red-700 sm:w-fit hover:bg-red-800 focus:ring-4 focus:outline-none focus:ring-red-300">
+					Send message
+				</button>
+			</form>
 		</div>
 	)
 }
