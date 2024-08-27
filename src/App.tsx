8fd5834c6d8bc0d3f9b23a5da26a55710ff8a7a7
LePeruvienn <koto91@free.fr>
2024-08-27 22:46:43 +0200

    Upading deploy

--- src/App.tsx
+++ src/App.tsx
@@ -12,12 +12,12 @@ function App() {
 	return (
 		<BrowserRouter>
 			<Routes>
-			  <Route path="/" element={<Home/>} />
-			  <Route path="/home" element={<Home/>} />
-			  <Route path="/book" element={<Book/>} />
-			  <Route path="/about" element={<About/>} />
-			  <Route path="/blog" element={<Blog/>} />
-			  <Route path="/contact" element={<Contact/>} />
+				<Route path="/" element={<Home/>} />
+				<Route path="/home" element={<Home/>} />
+				<Route path="/book" element={<Book/>} />
+				<Route path="/about" element={<About/>} />
+				<Route path="/blog" element={<Blog/>} />
+				<Route path="/contact" element={<Contact/>} />
 			</Routes>
 		</BrowserRouter>
 	)
