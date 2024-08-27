5eb722421b26d76a5d3f584ee37bbab503e1039d
LePeruvienn <koto91@free.fr>
2024-08-27 23:34:45 +0200

    fixing deploy .......

--- vite.config.ts
+++ vite.config.ts
@@ -3,9 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-	base: '/react-project/',
 	plugins: [react()],
-	build: {
-		sourcemap: false,  // Désactive la génération des fichiers .map
-	},
+	base: '/react-project/',
 })
