e35d203107a7ec0715990082a255f1277c41863a
LePeruvienn <koto91@free.fr>
2024-08-31 17:06:53 +0200

    Finished language switcher

--- src/components/general/Languages.tsx
+++ src/components/general/Languages.tsx
@@ -1,3 +1,4 @@
+import React from "react";
 import { useState } from "react";
 import { useTranslation } from "react-i18next";
 
@@ -14,6 +15,7 @@ function Languages () {
 	const handleLanguageChange = (lng: string) => {
 
 		i18n.changeLanguage (lng);
+		toggleDropdown ();
 	};
 
 	// Setting language list
@@ -33,6 +35,20 @@ function Languages () {
 		},
 	];
 
+	// Setting flags element list type
+	type FlagComponents = {
+
+		[key: string]: React.ReactElement;
+	};
+
+	// Creating flags element list
+	const flags: FlagComponents = {
+
+		en: <UkFlag/>,
+		fr: <FranceFlag/>,
+		es: <SpainFlag/>,
+	};
+
 	// State for dropdown
 	const [dropdownOpen, setDropdownOpen] = useState (false);
 
@@ -49,15 +65,17 @@ function Languages () {
 				type="button"
 				onClick={toggleDropdown}
 			> 
-				{i18n.language}
+				{ flags[i18n.language] }
 				<svg className="w-2.5 h-2.5 ms-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 10 6">
 					<path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="m1 1 4 4 4-4"/>
 				</svg>
 			</button>
 			{dropdownOpen && (
-				<div className="absolute z-10 bg-gray-800 divide-y divide-gray-100 rounded-lg shadow">
+				<div className="absolute z-10 bg-gray-800 bg-opacity-30 divide-y divide-gray-100 rounded-lg shadow">
 					<ul className="p-2">
-						{ languages.map ((lang) => (
+						{ languages
+							.filter (lang => lang.code !== i18n.language)
+							.map ((lang) => (
 							<li 
 								key={ lang.code }
 								onClick={() => (handleLanguageChange (lang.code))}
