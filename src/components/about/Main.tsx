f44b7ac0ca809823ea44a7dba1d7aaf9d560c9d0
LePeruvienn <koto91@free.fr>
2024-10-13 19:20:47 +0200

    Updating about section

--- src/components/about/Main.tsx
+++ src/components/about/Main.tsx
@@ -1,5 +1,8 @@
+import { useTranslation } from 'react-i18next';
 
 function Main () {
+
+	const { t } = useTranslation ();
 
 	return (
 		<div className="w-full h-auto py-20 bg-white">
@@ -11,16 +14,13 @@ function Main () {
 					Fabiola PINEL
 				</h1>
 					<p className="text-xl my-5">			
-						Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum hendrerit, enim eu imperdiet pretium, diam nibh fermentum massa, vitae efficitur urna orci sed turpis. Donec eget purus non risus rutrum suscipit. Aenean sollicitudin tincidunt sodales. In dapibus finibus sem, non finibus odio dapibus eu. Pellentesque nec laoreet lectus. In finibus velit urna, et convallis augue rutrum et. Etiam vitae ante eros.
+						{ t ("about_p1") }
 					</p>
 					<p className="text-xl my-5">			
-						Aenean pharetra, erat ut mattis fringilla, tortor neque laoreet leo, ac posuere lectus neque et tortor. Pellentesque id velit varius, molestie dui sed, efficitur eros. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Duis at turpis et lectus convallis convallis. Donec ut lorem et turpis aliquam finibus. Praesent ut rutrum lectus. Nam pellentesque elit felis, non lobortis mi feugiat quis. Quisque scelerisque arcu id nibh lacinia imperdiet. Vivamus justo orci, aliquet eget mi non, euismod condimentum nisi. Duis vestibulum orci ac lacus posuere semper.
+						{ t ("about_p2") }
 					</p>
 					<p className="text-xl my-5">			
-						Sed dolor nunc, aliquam ac pharetra non, lacinia sit amet odio. Vivamus iaculis tellus ante, nec vulputate augue posuere a. Quisque et finibus neque, ut consectetur mauris. Mauris elementum risus volutpat gravida hendrerit. Cras pretium erat imperdiet mauris porta, ac commodo tellus viverra. Duis nec turpis nec dolor sodales ultrices. Fusce suscipit dictum elit quis viverra. Ut molestie sapien leo, id finibus augue efficitur pulvinar. Phasellus at dui in ex commodo sollicitudin sed non arcu. Phasellus ligula elit, vehicula quis est sit amet, rutrum fermentum nisi. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Ut sit amet pharetra eros. Donec et metus sit amet metus posuere hendrerit ut vitae quam. Donec in facilisis quam. Integer scelerisque diam nec lectus tempor sollicitudin. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae;
-					</p>
-					<p className="text-xl my-5">			
-						Nulla vulputate odio arcu, porttitor condimentum diam pharetra eget. Mauris tristique molestie eros in ultrices. Morbi eleifend mollis risus vitae euismod. Maecenas ornare laoreet dictum. Vivamus molestie, sapien vel sollicitudin bibendum, massa odio semper elit, eu pellentesque mauris urna id libero. Nunc pellentesque interdum dolor. Sed efficitur lacus arcu, in molestie ligula porta a. Aliquam ut tincidunt velit. Vivamus malesuada tellus vel rhoncus feugiat. Sed interdum pulvinar sapien. Mauris sed ultricies nisl. 
+						{ t ("about_p3") }
 					</p>
 			</div>
 		</div>
